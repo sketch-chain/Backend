f37c572b1fb01d87f68f84c0d03c585a2ab0caef
huewilliams <4879sinwoo@naver.com>
2019-11-05 14:08:13 +0900

    ADD: facebook setting

---
+++ src/test/java/com/example/sketchchain/SketchChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sketchchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SketchChainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

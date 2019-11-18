c36ffc26d1f180d2e0c75536c4588a478d0b3bea
huewilliams <4879sinwoo@naver.com>
2019-11-18 23:28:26 +0900

    ADD: 프로필 사진 없는 사람인 경우 default 이미지로 처리

--- src/main/java/io/chain/sketch/model/social/KakaoProfile.java
+++ src/main/java/io/chain/sketch/model/social/KakaoProfile.java
@@ -26,6 +26,6 @@ public class KakaoProfile {
     private static class Properties {
         private String nickname;
         private String thumbnail_image;
-        private String profile_image;
+        private String profile_image = "https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half/public/blogs/334/2012/07/101524-98975.jpg?itok=LGLD7Rgi";
     }
 }

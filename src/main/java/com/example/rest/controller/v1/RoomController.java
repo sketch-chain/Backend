e3fb64f46342f62c3b854e428dabe3d8bb3b8d86
huewilliams <4879sinwoo@naver.com>
2019-11-18 10:29:44 +0900

    ADD: 공개 방/ 비밀 방 생성 API 분리

--- src/main/java/com/example/rest/controller/v1/RoomController.java
+++ src/main/java/com/example/rest/controller/v1/RoomController.java
@@ -5,35 +5,69 @@ import com.example.rest.advice.exception.UserNotFoundException;
 import com.example.rest.entity.Room;
 import com.example.rest.entity.User;
 import com.example.rest.model.response.CommonResult;
-import com.example.rest.model.response.SingleResult;
+import com.example.rest.model.response.ListResult;
 import com.example.rest.repo.RoomJpaRepo;
 import com.example.rest.repo.UserJpaRepo;
 import com.example.rest.service.ResponseService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
 @Api(tags = {"3. Room"})
 @RequiredArgsConstructor
+@RestController
 @RequestMapping(value = "/v1")
 public class RoomController {
     private final RoomJpaRepo roomJpaRepo;
     private final UserJpaRepo userJpaRepo;
     private final ResponseService responseService;
 
-    @ApiOperation(value = "게임 방 생성", notes = "새로운 게임 방을 생성한다. 게임 방제 중복 불가")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "게임 방 생성", notes = "새로운 공개 게임 방을 생성한다.")
     @PostMapping(value = "/room")
     public CommonResult createRoom(
             @ApiParam(value = "방 제목", required = true) @RequestParam String title,
-            @ApiParam(value = "비밀방", required = false) @RequestParam String  secret
+            @ApiParam(value = "진행 라운드 수", required = true) @RequestParam Integer round,
+            @ApiParam(value = "제한 시간", required = true) @RequestParam Integer limit
+            ) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String id = authentication.getName();
+        User user = userJpaRepo.findByUid(id).orElseThrow(UserNotFoundException::new);
+
+        Optional<Room> room = roomJpaRepo.findByTitle(title);
+        if(room.isPresent())
+            throw new RoomExistException();
+
+        roomJpaRepo.save(Room.builder()
+                .title(title)
+                .isSecret(false)
+                .leaderIdx(id)
+                .allPeople(1)
+                .readyPeople(1)
+                .isPlaying(false)
+                .limitTime(limit)
+                .round(round)
+                .build());
+
+        return responseService.getSuccessResult();
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "비밀 게임 방 생성", notes = "새로운 게임 방을 생성한다. 게임 방제 중복 불가")
+    @PostMapping(value = "/room/secret")
+    public CommonResult createRoom(
+            @ApiParam(value = "방 제목", required = true) @RequestParam String title,
+            @ApiParam(value = "방 비밀번호") @RequestParam String password,
+            @ApiParam(value = "진행 라운드 수", required = true) @RequestParam Integer round,
+            @ApiParam(value = "제한 시간", required = true) @RequestParam Integer limit
     ) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String id = authentication.getName();
@@ -43,23 +77,26 @@ public class RoomController {
         if(room.isPresent())
             throw new RoomExistException();
 
-        if(secret.equals("true")) {
-            roomJpaRepo.save(Room.builder()
-                    .title(title)
-                    .isSecret(true)
-                    .leaderIdx(id)
-                    .allPeople(1)
-                    .readyPeople(1)
-                    .build());
-        } else {
-            roomJpaRepo.save(Room.builder()
-                    .title(title)
-                    .isSecret(false)
-                    .leaderIdx(id)
-                    .allPeople(1)
-                    .readyPeople(1)
-                    .build());
-        }
+        roomJpaRepo.save(Room.builder()
+                .title(title)
+                .isSecret(true)
+                .password(password)
+                .leaderIdx(id)
+                .allPeople(1)
+                .readyPeople(1)
+                .isPlaying(false)
+                .limitTime(limit)
+                .round(round)
+                .build());
         return responseService.getSuccessResult();
+    }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
+    @ApiOperation(value = "모든 게임 방 리스트 조회", notes = "모든 게임방을 조회함.")
+    @GetMapping("/rooms")
+    public ListResult<Room> getAllRooms() {
+        return responseService.getListResult(roomJpaRepo.findAll());
     }
 }
